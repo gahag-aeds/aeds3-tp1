5ee8a7f62f07dd8951dae2f9a7fdbb18584be813
gahag <gabriel.s.b@live.com>
2017-09-24 03:49:53 -0300

    Added test script.

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+import tptester
+
+
+tests_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+tptester.tp_test(
+  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+  os.getcwd() + '/kontest',
+  
+  input_file = lambda ix: tests_dir + '/data/toys/in_' + str(ix) + '.in',
+  answer_file = lambda ix: tests_dir + '/data/toys/in_' + str(ix) + '.out',
+  
+  args = lambda ix: [ ],
+  stdin = lambda ifile: ifile,
+  output = lambda ofile, stdout: stdout
+)
